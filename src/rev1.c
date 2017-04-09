8fef957a29e8f9f3367b6d2fbc45e5d45ff9c3c1
alexwh <alexwh@users.noreply.github.com>
2017-04-09 17:59:52 +0100

    use argv instead of stdin

--- src/rev1.c
+++ src/rev1.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
-{
-  char *flag = "EXAMPLECTF{ALWaYS_TrY_Str1nGz}";
-  char pass[256];
-  printf("Input password: \n");
-  scanf("%s", &pass);
+int main(int argc, char **argv) {
+    char *flag = "EXAMPLECTF{ALWaYS_TrY_Str1nGz}";
 
-  if (strcmp(flag, pass) == 0)
-    {
-      printf("That's the flag!\n");
-      return 0;
+    if(argc > 1) {
+        if(strcmp(flag, argv[1]) == 0) {
+            printf("That's the flag!\n");
+            return 0;
+        }
+        else {
+            printf("That's not the flag.\n");
+            return 1;
+        }
     }
-  else
-    {
-      printf("That's not the flag.\n");
-      return 1;
+    else {
+        printf("%s%s%s", "usage: ", argv[0], " flag");
     }
 }
--- src/rev2.c
+++ src/rev2.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
-{
-  char flag[] = "EXAMPLECTF{C_0ptImizat1inZ_Sp1iT_up_A_BiT}";
-  char pass[256];
-  printf("Input password: \n");
-  scanf("%s", &pass);
+int main(int argc, char **argv) {
+    char flag[] = "EXAMPLECTF{C_0ptImizat1inZ_Sp1iT_up_A_BiT}";
 
-  if (strcmp(flag, pass) == 0)
-    {
-      printf("That's the flag!\n");
-      return 0;
+    if(argc > 1) {
+        if (strcmp(flag, argv[1]) == 0)
+        {
+            printf("That's the flag!\n");
+            return 0;
+        }
+        else
+        {
+            printf("That's not the flag.\n");
+            return 1;
+        }
     }
-  else
-    {
-      printf("That's not the flag.\n");
-      return 1;
-    }
+        else {
+            printf("%s%s%s", "usage: ", argv[0], " flag");
+        }
 }
