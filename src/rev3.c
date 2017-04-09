8b0df1bfda0214c8fd9d06018b038e2cec0a3690
alexwh <alexwh@users.noreply.github.com>
2017-04-09 18:13:32 +0100

    stdin to argv

--- src/rev3.c
+++ src/rev3.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
-{
-  char *flag = "RGTBSEzj2x^G0c0mf^hm^O030m^RhFgS|";
-  int i;
-  int n = strlen(flag);
-  char output[n];
-  char pass[256];
-  printf("Input password: \n");
-  scanf("%s", &pass);
+int main(int argc, char **argv) {
+    char *flag = "RGTBSEzj2x^G0c0mf^hm^O030m^RhFgS|";
+    int i;
+    int n = strlen(flag);
+    char output[n];
 
-  for(i = 0; i < n; i++) {
-      output[i]=flag[i]+1;
-  }
-
-  if (strcmp(output, pass) == 0)
-    {
-      printf("That's the flag!\n");
-      return 0;
+    for(i = 0; i < n; i++) {
+        output[i]=flag[i]+1;
     }
-  else
-    {
-      printf("That's not the flag.\n");
-      return 1;
+
+    if(argc > 1) {
+        if (strcmp(output, argv[1]) == 0)
+        {
+            printf("That's the flag!\n");
+            return 0;
+        }
+        else
+        {
+            printf("That's not the flag.\n");
+            return 1;
+        }
+    }
+    else {
+        printf("%s%s%s", "usage: ", argv[0], " flag");
     }
 }
